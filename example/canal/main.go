11c84640b1eb426ade9576e42d124ea6330a54ba
xiaopeng deng <laik.lj@me.com>
2019-05-05 20:28:36 +0800

    wocao

---
+++ example/canal/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"canal"
+	"log"
+	"os"
+)
+
+type printer struct{}
+
+func (p *printer) Command(cmd *canal.Command) error {
+	log.Printf("cmd=%v\n", cmd)
+	return nil
+}
+
+func main() {
+	log.SetOutput(os.Stdout)
+	repl, err := canal.NewCanal(canal.NewConfig("127.0.0.1:6379"))
+	if err != nil {
+		panic(err)
+	}
+	defer repl.Close()
+
+	if err := repl.Run(&printer{}); err != nil {
+		panic(err)
+	}
+}
